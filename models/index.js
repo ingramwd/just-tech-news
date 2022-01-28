f555f27174cda28fa49336d86931113d01e78f63
ingramwd <Ingramwd01@gmail.com>
2022-01-28 13:43:47 -0500

    new branch for comment feature

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./Vote');
 
 // create associations aka JOINING TABLES 
 //this will be a ONE to MANY relationship because ONE user can have MANY post
@@ -12,4 +13,34 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+//creates another association aka JOINING TABLES
+//but this is a MANY to MANY relationship because MANY USERS can like MANY post
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
