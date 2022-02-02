354554526725e93a2552a6b2bbc2f63e32549cb3
ingramwd <Ingramwd01@gmail.com>
2022-02-01 20:31:53 -0500

    finished homepage

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//uses our styling sheet in public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //turn on the routes
 app.use(routes);
